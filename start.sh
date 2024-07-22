a3ed1243ddc3c5bcc230623f0b5cac4f3811d4d0
GoldcoFredo <apalacios@goldco.mx>
2024-07-21 22:03:57 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Run the Flask application using Flask CLI
-flask run --host=0.0.0.0 --port=8080
+# Run the Flask application using Flask CLI and default Render port
+flask run --host=0.0.0.0 --port=${PORT:-10000}
